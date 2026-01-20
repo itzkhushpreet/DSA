6f882cf8ed2e8da58b34d1990d64e39902fd1364
Khushpreet Kaur <itzkhushpreetkaur0411@gmail.com>
2026-01-20 20:54:28 +0530

    abx

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+// from arrays//
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<array>
+#include<vector>
+#include<string>
+using namespace std;
+int main(){
+   cout<<"hello world"<<endl; 
+}
+// cout<<"hello world"<<endl;
