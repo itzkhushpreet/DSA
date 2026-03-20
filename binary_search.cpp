ac91d2d877352e640cb047a5d4aba791d5d53ca7
Khushpreet Kaur <itzkhushpreetkaur0411@gmail.com>
2026-03-20 18:37:47 +0530

    another question done

--- binary_search.cpp
+++ binary_search.cpp
@@ -53,24 +53,55 @@ using namespace std;
 //     return 0;
 
 // }
-int rotation(vector<int>&arr){
-    // vector<int>arr;
-    int low=0;
-    int high=arr.size()-1;
-    while(low<high){
+// to find the number of rotations in an sorrted array
+// int rotation(vector<int>&arr){
+//     // vector<int>arr;
+//     int low=0;
+//     int high=arr.size()-1;
+//     while(low<high){
+//         int mid=(low+high)/2;
+//         if(arr[mid]<arr[high]){
+//             high=mid-1;
+//         }
+//         else{
+//             low=mid+1;
+//         }
+
+//     }
+//     return low;
+// }
+// int main(){
+//     vector<int>arr={3,4,5,1,2};
+//     int rotate_no=rotation(arr);
+//     cout<<rotate_no<<endl;
+//     return 0;
+// }
+// to search the single element that is preseent in a sorted array using O{log n}
+int single_element(vector<int>&arr){
+    int n=arr.size();
+    if(n==1)return arr[0];
+    if(arr[0]!=arr[1])return arr[0];
+    if(arr[n-1]!=arr[n-2]) return arr[n-1];
+    int low=1;
+    int high=n-2;
+    while(low<=high){
         int mid=(low+high)/2;
-        if(arr[mid]<arr[high]){
-            high=mid-1;
+        if( arr[mid]!=arr[mid-1]&& arr[mid]!=arr[mid+1]){
+            return arr[mid] ;
+        }
+        if((mid%2==1 && arr[mid-1]==arr[mid]) || 
+        (mid%2==0 && arr[mid]==arr[mid+1])){
+            low=mid+1;
         }
         else{
-            low=mid+1;
+            high=mid-1;
         }
 
     }
-    return low;
+    return -1;
 }
 int main(){
-    vector<int>arr={3,4,5,1,2};
-    int rotate_no=rotation(arr);
-    cout<<rotate_no<<endl;
+    vector <int>arr={1,1,2,2,3,3,4,5,5,6,6};
+    int ans=single_element(arr);
+    cout<<ans<<endl;
 }
