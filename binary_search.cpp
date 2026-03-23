0a51e97cd200a4ab2df803ceb818368ed2c24d41
Khushpreet Kaur <itzkhushpreetkaur0411@gmail.com>
2026-03-23 13:50:09 +0530

    another binary search question being done

--- binary_search.cpp
+++ binary_search.cpp
@@ -77,31 +77,76 @@ using namespace std;
 //     return 0;
 // }
 // to search the single element that is preseent in a sorted array using O{log n}
-int single_element(vector<int>&arr){
-    int n=arr.size();
-    if(n==1)return arr[0];
-    if(arr[0]!=arr[1])return arr[0];
-    if(arr[n-1]!=arr[n-2]) return arr[n-1];
-    int low=1;
-    int high=n-2;
+// int single_element(vector<int>&arr){
+//     int n=arr.size();
+//     if(n==1)return arr[0];
+//     if(arr[0]!=arr[1])return arr[0];
+//     if(arr[n-1]!=arr[n-2]) return arr[n-1];
+//     int low=1;
+//     int high=n-2;
+//     while(low<=high){
+//         int mid=(low+high)/2;
+//         if( arr[mid]!=arr[mid-1]&& arr[mid]!=arr[mid+1]){
+//             return arr[mid] ;
+//         }
+//         if((mid%2==1 && arr[mid-1]==arr[mid]) || 
+//         (mid%2==0 && arr[mid]==arr[mid+1])){
+//             low=mid+1;
+//         }
+//         else{
+//             high=mid-1;
+//         }
+
+//     }
+//     return -1;
+// }
+// int main(){
+//     vector <int>arr={1,1,2,2,3,3,4,5,5,6,6};
+//     int ans=single_element(arr);
+//     cout<<ans<<endl;
+// }
+// simple square root problem
+
+// int sqrt(int n){
+//     int low=0;
+//     int high=n;
+//     while(low<=high){
+//         int mid=(low+high)/2;
+//         if(mid*mid==n)return mid;
+//         if(mid*mid<n){
+//             low=mid+1;
+//         }
+//         else{
+//             high=mid-1;
+//         }
+//     }
+//     return -1;
+// }
+// int main(){
+//     int n=145;
+//     int ans=sqrt(n);
+//     cout<<ans<<endl;
+// }
+// bit complicating the same problem for 145 like number
+int sqrt(int n){
+    int low=0;
+    int high=n;
+    int ans=0;
     while(low<=high){
         int mid=(low+high)/2;
-        if( arr[mid]!=arr[mid-1]&& arr[mid]!=arr[mid+1]){
-            return arr[mid] ;
-        }
-        if((mid%2==1 && arr[mid-1]==arr[mid]) || 
-        (mid%2==0 && arr[mid]==arr[mid+1])){
+        if(mid*mid==n)return mid;
+        if(mid*mid<n){
+            ans=mid;
             low=mid+1;
         }
         else{
             high=mid-1;
         }
-
     }
-    return -1;
+    return ans;
 }
 int main(){
-    vector <int>arr={1,1,2,2,3,3,4,5,5,6,6};
-    int ans=single_element(arr);
+    int n=145;
+    int ans=sqrt(n);
     cout<<ans<<endl;
 }
