346d248373ec0e57c5b9c56feeb324b18f1f33f8
Khushpreet Kaur <itzkhushpreetkaur0411@gmail.com>
2026-03-28 16:16:53 +0530

    another questions added

--- binary_search.cpp
+++ binary_search.cpp
@@ -128,25 +128,168 @@ using namespace std;
 //     cout<<ans<<endl;
 // }
 // bit complicating the same problem for 145 like number
-int sqrt(int n){
-    int low=0;
-    int high=n;
-    int ans=0;
+// int sqrt(int n){
+//     int low=0;
+//     int high=n;
+//     int ans=0;
+//     while(low<=high){
+//         int mid=(low+high)/2;
+//         if(mid*mid==n)return mid;
+//         if(mid*mid<n){
+//             ans=mid;
+//             low=mid+1;
+//         }
+//         else{
+//             high=mid-1;
+//         }
+//     }
+// //     return ans;
+// // }
+// // int main(){
+// //     int n=145;
+// //     int ans=sqrt(n);
+// //     cout<<ans<<endl;
+// // }
+// int nthroot(int n,int m){
+//     int low=0;
+//     int high=m;
+//     while(low<=high){
+//         int mid=(low+high)/2;
+//         long long ans=1;
+//         for(int i=0;i<n;i++){
+//             ans*=mid;
+//             if(ans>m)break;
+//         }
+//         if(ans==m)return mid;
+//         if(ans<m){
+//              low=mid+1;
+            
+//         }
+//         else{
+//        high=mid-1;
+//         }
+//     }
+//     return -1;
+// }
+// int main(){
+//     int m=64;
+//     int n=4;
+//     int ans=nthroot(n,m);
+//     cout<<ans;
+// }
+
+// class Solution {
+// public:
+//     int findmax(vector<int> &piles){
+//         int maxi=INT_MIN;
+//         int n=piles.size();
+//         for(int i=0;i<n;i++){
+//             maxi=max(maxi,piles[i]);
+//         }
+//         return maxi;
+//     }
+//     long long calculateTotalHours(vector<int>&piles,int speed){
+//         long long  totalH=0;
+//         int n=piles.size();
+//         for(int i=0;i<n;i++){
+//             // totalH+=ceil((double)piles[i]/(double)hourly);
+//             // totalH += (piles[i] + hourly - 1) / hourly;
+//             totalH+=ceil((double)piles[i]/speed);
+//             // if(totalH > h) return totalH;
+//         }
+//         return totalH;
+//     }
+//     int minEatingSpeed(vector<int>& piles, int h) {
+//         int low=1;
+//         int high=findmax(piles);
+//         int ans=findmax(piles);
+//         while(low<=high){
+//             int mid=(low)+(high-low)/2;
+//             int totalH=calculateTotalHours(piles,mid);
+//             if(totalH<=h){
+//                 ans=mid;
+//                 high=mid-1;
+//             }
+//             else{
+//                 low=mid+1;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
+// Book allocaation problem
+int calculate_students(vector<int> &arr,int mid){
+    int pages=0;
+    int students=1;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]+pages>mid){
+            students++;
+            pages=arr[i];
+        }
+        else{
+            pages+=arr[i];
+        }
+    }
+    return students;
+}
+int bk_allocation(vector<int>&arr,int m){
+    int low=*max_element(arr.begin(),arr.end());
+    int n=arr.size();
+    int high=accumulate(arr.begin(),arr.end(),0);
+    int ans=-1;
+    if(m>n)return -1;
     while(low<=high){
         int mid=(low+high)/2;
-        if(mid*mid==n)return mid;
-        if(mid*mid<n){
-            ans=mid;
+        int book=calculate_students(arr,mid);
+        if(book>m){
             low=mid+1;
         }
         else{
+            ans=mid;
             high=mid-1;
         }
     }
     return ans;
 }
 int main(){
-    int n=145;
-    int ans=sqrt(n);
+    vector<int>arr={12,34,67,90};
+    int m=2;
+    int ans=bk_allocation(arr,m);
     cout<<ans<<endl;
 }
+// split array quesiton
+class Solution {
+public:
+    int cnt_subarrays(vector<int> &arr,int mid){
+        int cnt=1;
+        int sum=0;
+        for(int i=0;i<arr.size();i++){
+            if(sum+arr[i]>mid){
+                cnt++;
+                sum=arr[i];
+            }
+            else{
+                sum+=arr[i];
+            }
+        }
+        return cnt;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int required=cnt_subarrays(nums,mid);
+            if(required<=k){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
